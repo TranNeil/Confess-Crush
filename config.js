6b74ce811a6cdab8cbe1b759f8018eb57dd94e51
TranNeil <139496819+TranNeil@users.noreply.github.com>
2023-07-14 01:40:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,5 +16,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Đừng có lì nữa là mỗi ngày có trà sữa uống.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.facebook.com/hoangkhang.tran.3914'
 }
